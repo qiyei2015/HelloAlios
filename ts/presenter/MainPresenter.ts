1c78bee3be447730e306a948419cea517c3f5568
xiangwei.han <xiangwei.han@faw-vw.com>
2021-02-22 16:17:05 +0800

    添加点击事件示例

--- ts/presenter/MainPresenter.ts
+++ ts/presenter/MainPresenter.ts
@@ -4,6 +4,7 @@
 
 import TextView = require("@ali/caf-types/yunos/ui/view/TextView");
 import Presenter = require("yunos/appmodel/Presenter");
+import TapRecognizer = require('yunos/ui/gesture/TapRecognizer')
 
 const TAG = "MainPresenter"
 class MainPresenter extends Presenter {
@@ -14,7 +15,11 @@ class MainPresenter extends Presenter {
 
     onViewAttached(){
         let tv:TextView = this.view.findViewById("tv") as TextView
-        tv.text = "1234"
+        tv.text = "1234383883"
+        tv.addGestureRecognizer(new TapRecognizer())
+        tv.on("tap",event => {
+            log.I(TAG,"tv click")
+        })
     }
 
     onDestroy(){
