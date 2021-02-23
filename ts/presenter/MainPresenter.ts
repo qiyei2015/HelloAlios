91b540bd2415621981b1de0d03f522c2b90a1c3d
xiangwei.han <xiangwei.han@faw-vw.com>
2021-02-23 15:47:46 +0800

    增加页面跳转

--- ts/presenter/MainPresenter.ts
+++ ts/presenter/MainPresenter.ts
@@ -2,15 +2,26 @@
 "use sound";
 
 
+import Router = require("@ali/caf-types/yunos/appmodel/Router");
 import TextView = require("@ali/caf-types/yunos/ui/view/TextView");
 import Presenter = require("yunos/appmodel/Presenter");
 import TapRecognizer = require('yunos/ui/gesture/TapRecognizer')
 
 const TAG = "MainPresenter"
+let router:Router
 class MainPresenter extends Presenter {
 
+    router:Router
+
     onCreate(){
+        let context = this.context
+
+        router = this.context.router;
+
         log.I(TAG,"onCreate");
+        log.I(TAG,"path=" + context.routeInfo.path)
+        log.I(TAG,"query=" + context.routeInfo.query)
+        log.I(TAG,"data=" + context.data)
     }
 
     onViewAttached(){
@@ -19,6 +30,7 @@ class MainPresenter extends Presenter {
         tv.addGestureRecognizer(new TapRecognizer())
         tv.on("tap",event => {
             log.I(TAG,"tv click")
+            router.navigate("home")
         })
     }
 
